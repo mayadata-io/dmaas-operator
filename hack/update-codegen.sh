f9259b6dc4e524e3b865fbf386df9aa4e9b2b059
mynktl <mayank.patel@mayadata.io>
2020-05-20 20:31:53 +0530

    adding crd for dmaas-operator
    Signed-off-by: mynktl <mayank.patel@mayadata.io>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -30,3 +30,10 @@ ${GOPATH}/src/k8s.io/code-generator/generate-groups.sh all \
 	"mayadata.io:v1alpha1" \
 	--output-base ../../..	\
 	--go-header-file ./hack/boilerplate.go.txt
+
+controller-gen \
+	crd:crdVersions=v1beta1,preserveUnknownFields=false,trivialVersions=true \
+	output:dir=./pkg/generated/crds/manifests \
+	paths=./pkg/apis/mayadata.io/v1alpha1/...
+
+#go-bindata -ignore=\\.go  -o pkg/generated/crds/bindata.go -pkg crds ./pkg/generated/crds/manifests/...
